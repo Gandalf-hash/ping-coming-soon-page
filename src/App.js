b91a9a18da5ebdfbf01ec7c20168027860c55b3c
Gandals-hash <phiritshepiso99@gmail.com>
2023-12-11 16:27:21 +0200

    feat: image illustration

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import { AppEmailInput } from "./components/email-input";
 
 function App() {
   return (
-    <main className="page-container">
-      <div className="logo">
-        <img src="/images/logo.svg" alt="" />
-      </div>
-      <div>
-        <h1>
-          We are lunching <span>soon!</span>
-        </h1>
-        <h3>Subscribe and get notified</h3>
-      </div>
-      <div>
-        <AppEmailInput />
-      </div>
-    </main>
+    <>
+      <main className="page-container">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="" />
+        </div>
+        <div>
+          <h1>
+            We are lunching <span>soon!</span>
+          </h1>
+          <h3>Subscribe and get notified</h3>
+        </div>
+        <div>
+          <AppEmailInput />
+        </div>
+        <div className="illustration-container">
+          <img src="/images/illustration-dashboard.png" alt="Illustration" />
+        </div>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
