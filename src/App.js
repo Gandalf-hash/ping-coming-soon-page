54bc6b8b3f030192b867cee2f8e0ea357be9fcc9
Gandals-hash <phiritshepiso99@gmail.com>
2023-12-11 11:57:55 +0200

    proj set up and folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="page-container">
+      <div>
+        <img src="/images/logo.svg" alt="" />
+      </div>
+      <div>
+        <h1>
+          We are lunching <span>soon!</span>
+        </h1>
+        <h3>Subscribe and get notified</h3>
+      </div>
+      <div></div>
+    </main>
   );
 }
 
