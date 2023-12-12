0f718b88fdbccfd8c6819bf0a32493b07c683946
Gandals-hash <phiritshepiso99@gmail.com>
2023-12-12 14:48:07 +0200

    refactor: remove unused styling

--- src/components/email-input.jsx
+++ src/components/email-input.jsx
@@ -1,10 +1,61 @@
 import React from "react";
 
-export const AppEmailInput = () => {
+export const AppEmailInput = ({ disabled = false }) => {
+  const [error, setError] = React.useState(null);
+  const [email, setEmail] = React.useState("");
+
+  const isValidEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+  const handleBlur = () => {
+    if (!isValidEmail(email)) {
+      setError("Valid Email Required");
+    } else {
+      setError(null);
+    }
+  };
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+    if (error) {
+      setError(null);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!isValidEmail(email)) {
+      setError("Valid Email Required");
+    }
+  };
+
+  const inputStyle = {
+    backgroundColor: error ? "#f2ddda" : "",
+    color: error ? "#ed614c" : "",
+    borderColor: error ? "#FF0000" : "",
+  };
   return (
-    <div className="input-container">
-      <input type="email" name="email" value={null} />
-      <button>Notify me</button>
-    </div>
+    <form className="input-container" onSubmit={handleFormSubmit}>
+      <div>
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email address..."
+          style={inputStyle}
+          value={email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+      </div>
+      <div className="button-div">
+        <button
+          className={` ${disabled ? "error-state" : ""}`}
+          type="submit"
+          disabled={!email}
+        >
+          Notify me
+        </button>
+      </div>
+    </form>
   );
 };
